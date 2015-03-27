2a80bb83f7d494d24dbd9bac3f6f74540c951eb4
Mark Marzeotti <mark@axionmedialab.com>
2015-03-27 16:59:01 -0400

    back to tap

--- www/js/index.js
+++ www/js/index.js
@@ -81,12 +81,8 @@ jQuery(document).ready(function($) {
 			addObstacle();
 		}
 	}, 1750);*/
-	/*var stage = document.getElementById("stage");
+	var stage = document.getElementById("stage");
 	if ($('.sprite').hasClass('animating')) {} else {
 		stage.addEventListener('touchstart', jump, false);
-	}*/
-	var element = document.getElementById("stage");
-	Hammer(element).on("swipeup", function() {
-		alert('meow');
-	});
+	}
 });
