b1320795bc639f936a3e29a34081304966e939ac
Mark Marzeotti <mark@axionmedialab.com>
2015-03-30 10:26:49 -0400

    jump physics

--- www/js/index.js
+++ www/js/index.js
@@ -60,10 +60,10 @@ function jump() {
 	if ($('.sprite').hasClass('animating')) {} else {
 		$('.sprite').addClass('animating').animate({
 		    bottom: "+=120px"
-		}, 300, function() {
+		}, 400, function() {
 		    $('.sprite').animate({
 			    bottom: "-=120px"
-			}, 200, function() {
+			}, 300, function() {
 				$(this).removeClass('animating');
 			});
 		});
