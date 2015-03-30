bc324de593cfb943dab22a236f9d88c3c7a1e7e1
Mark Marzeotti <mark@axionmedialab.com>
2015-03-30 15:01:13 -0400

    cubic easing sprite size

--- www/js/index.js
+++ www/js/index.js
@@ -60,10 +60,10 @@ function jump() {
 	if ($('.sprite').hasClass('animating')) {} else {
 		$('.sprite').removeClass('floating').addClass('animating').animate({
 		    bottom: "+=100px"
-		}, 900, 'easeOutSine', function() {
+		}, 900, 'easeOutCubic', function() {
 		    $('.sprite').animate({
 			    bottom: "-=100px"
-			}, 600, 'linear', function() {
+			}, 600, 'easeinCubic', function() {
 				$(this).removeClass('animating').addClass('floating');
 			});
 		});
